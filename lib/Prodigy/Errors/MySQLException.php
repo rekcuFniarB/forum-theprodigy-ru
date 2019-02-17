2b1dcdb784e0cf5946cdae90f63ee70c5fc6d118
BrainFucker <retratserif@gmail.com>
2019-02-17 22:10:31 +0300

    Show trace in mysql exceptions.

--- lib/Prodigy/Errors/MySQLException.php
+++ lib/Prodigy/Errors/MySQLException.php
@@ -4,7 +4,7 @@ namespace Prodigy\Errors;
 class MySQLException extends \Exception
 {
     public function __toString() {
-        return __CLASS__ . ": [{$this->code}] {$this->message}\n";
+        return __CLASS__ . ": [{$this->code}] {$this->message}\nTRACE:\n" . $this->getTraceAsString() . "\n";
     }
 }
 
