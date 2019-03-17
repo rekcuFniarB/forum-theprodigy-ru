db2159f1a7147efd3e9fae544bc76d721bf53746
BrainFucker <retratserif@gmail.com>
2019-03-18 02:23:15 +0300

    Forgot to add delete message template.

---
+++ templates/thread/deletemsg.template.php
@@ -0,0 +1,18 @@
+        <h2><?= $this->title ?></h2>
+        <h3><a href="<?= SITE_ROOT ?>/<?= $this->msgid ?>/"><?= $this->esc('subject') ?></a></h3>
+        <div><?= $this->doUBBC($this->body) ?></div>
+        
+        
+        <div style="text-align: right;">
+          Author:
+          <?php if($this->poster['guest']): ?>
+              <?= $this->esc($this->poster['name']) ?>
+          <?php else: ?>
+              <a href="<?= SITE_ROOT ?>/people/<?= urlencode($this->poster['name']) ?>/"><?= $this->esc($this->poster['realName']) ?></a>,
+          <?php endif; ?>
+          <i><?= $this->date ?></i>
+          <form action="." method="POST">
+            <input type="hidden" name="sc" value="<?= $this->sessid ?>">
+            <input type="text" name="mdfrzn" placeholder="<?= $this->locale->mdfrznplchldr ?>" size="64" required> <input type="submit" value="<?= $this->locale->txt(31) ?>"> 
+          </form>
+        </div>
